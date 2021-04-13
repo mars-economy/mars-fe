324ee63ac9415cbfbccc92c9e69c7aa340e6edf1
anastasia.andryushina <anastasia.andryushina@crplab.ru>
2021-04-13 18:55:36 +0300

    MARS-27: Timeline component

--- src/views/slides/pages/TimeLine/components/TimeLineIcon.vue
+++ src/views/slides/pages/TimeLine/components/TimeLineIcon.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="vf__flex timeline-icon-block vf__flex-dir--column">
-    <div :class="isLast ? 'last' : ''"
-         class="timeline-block-icon vf__flex vf__align-items-center vf__justify-content-center">
-      <div class="vf__flex vf__align-items-center vf__justify-content-center" v-html="icon"></div>
-    </div>
-    <div class="timeline-block-month">{{ month }}</div>
-  </div>
+    <flex-row class="timeline-icon-container" :class="{'active' : active}">
+      <flex-row class="timeline-icon" :class="{'active' : active}">
+        <div v-html="icon"></div>
+      </flex-row>
+    </flex-row>
 </template>
 
 <script>
@@ -13,56 +11,27 @@ export default {
   name: 'TimeLineIcon',
   props: {
     icon: String,
-    month: String,
-    isLast: Boolean
+    active: Boolean
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .timeline-icon-block {
-    position: relative;
+
+  .timeline-icon-container, .timeline-icon {
+    background: rgba($color-primary, 0.05);
+    border: 1px solid rgba($color-primary, 0.05);
+    box-sizing: border-box;
+    backdrop-filter: blur(20px);
+    border-radius: 50%;
+    padding: 0.3rem;
     width: 100%;
+    height: 100%;
     justify-content: center;
+    align-items: center;
 
-    & > div {
-      display: flex;
-      align-self: center;
+    &.active {
+      border: 1px solid rgba($color-primary, 0.5);
     }
-
-    .timeline-block-icon {
-      & > div {
-        width: 64px;
-        height: 64px;
-        background-repeat: no-repeat;
-        background-image: url("../../../../../assets/images/timeline-round.svg");
-        margin-bottom: 30px;
-      }
-
-      &:not(.last)::after {
-        content: " ";
-        display: block;
-        position: absolute;
-        height: 10px;
-        background-image: url("../../../../../assets/images/dashed-line.svg");
-        width: 78%;
-        left: 60%;
-        top: calc(50% - 30px);
-      }
-    }
-
-    .timeline-block-month {
-      font-family: Karla;
-      font-style: normal;
-      font-weight: bold;
-      font-size: 12px;
-      line-height: 14px;
-      /* identical to box height */
-
-      letter-spacing: -0.02em;
-      text-transform: uppercase;
-    }
-
   }
-
 </style>
