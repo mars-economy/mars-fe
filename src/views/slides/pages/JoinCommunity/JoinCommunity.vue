2b08ca087de67952dbc0921c0f0c14a52c68b1f3
Anastasia Andryushina <anastasia.andryushina@crplab.ru>
2021-04-18 02:05:14 +0300

    MARS-30: Join the Community slide

---
+++ src/views/slides/pages/JoinCommunity/JoinCommunity.vue
@@ -0,0 +1,53 @@
+<template>
+  <section id="join-the-community" name="join the community">
+    <div class="join-community-container">
+      <div class="h2">Join the community</div>
+      <flex-row class="join-community-content">
+        <div class="subscribe-container">
+          <div class="subscribe-text">
+            Learn more about Mars Economy, chat with the team and other community members, have a say in shaping the roadmap, and join us on this journey to the Red Planet. Be a part of the future.
+          </div>
+          <SubscribeForm></SubscribeForm>
+        </div>
+        <div class="contacts-container">
+          <Contacts></Contacts>
+        </div>
+
+      </flex-row>
+    </div>
+  </section>
+</template>
+
+<script>
+import SubscribeForm from './components/SubscribeForm'
+import Contacts from './components/Contacts'
+export default {
+  name: 'JoinCommunity',
+  components: {
+    Contacts,
+    SubscribeForm
+  }
+}
+</script>
+
+<style scoped>
+
+  .join-community-container {
+    padding: 0 17.5rem 0 20.3rem;
+    text-align: left;
+  }
+
+  .h2 {
+    margin-bottom: 1.25rem;
+  }
+
+  .subscribe-container {
+    width: 57%;
+    margin-right: auto;
+  }
+
+  .subscribe-text {
+    margin-bottom: 3rem;
+  }
+
+</style>
---
+++ src/views/slides/pages/JoinCommunity/components/SubscribeForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="subscribe-form-card">
+    <div class="card-label">
+      Stay in the know!
+    </div>
+    <div class="card-body">
+      <div class="card-text">
+        We’ll send out period updates, newsletters, etc.
+      </div>
+    </div>
+    <flex-row>
+      <div class="form-field">
+        <input type="email" placeholder="E-mail">
+      </div>
+      <Button color="primary" name="Notify me" link=""></Button>
+    </flex-row>
+
+  </div>
+</template>
+
+<script>
+import Button from '../../../../../components/buttons/Button'
+export default {
+  name: 'SubscribeForm',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+  .subscribe-form-card {
+    @extend %card;
+    padding: 2.37rem 1.25rem 1.25rem 1.25rem;
+  }
+
+  .card-label {
+    @extend %card-label;
+    top: 0;
+    left: 1.25rem;
+    border-radius: 0 0 $radius-base $radius-base;
+  }
+  .card-text {
+    margin-bottom: 1em;
+  }
+  .form-field {
+    flex-grow: 1;
+    margin-right: 1em;
+    input {
+      font-family: $font-family-base;
+      font-size: 0.75rem;
+      line-height: 1em;
+      padding: 0.5em 1.6em;
+      min-height: 2.5rem;
+      width: 100%;
+      box-sizing: border-box;
+      color: $white;
+      backdrop-filter: blur(20px);
+      border-radius: $radius-base;
+      background: rgba($white, 0.05);
+      border: 1px solid rgba($white, 0.05);
+      font-weight: bold;
+      &::placeholder {
+        color: $white;
+        opacity: 1;
+        text-transform: uppercase;
+      }
+    }
+  }
+
+</style>
